3b73e7dd9339c41bf4e737edb9d12dc42a66f026
Zou <zouzijun@gmail.com>
2024-10-16 23:50:08 +0800

    remove trailing slash

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new socketIo(server, {
   cors: {
-    origin: 'https://poker-app-client.pages.dev/', // Allow requests from this origin
+    origin: 'https://poker-app-client.pages.dev', // Allow requests from this origin
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type'],
     credentials: true,
